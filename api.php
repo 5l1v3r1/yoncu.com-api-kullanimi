24f095ddd1b5bbb02c21fa21f706e2d4f56512b1
HasanAtilan <zb-sunucum@hotmail.com>
2019-02-05 20:46:35 +0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,52 @@
+<?php
+// YONCU.COM APİ V1 CODİNG BY HASAN ATİLAN.
+// APİ KULLANMAK İÇİN YÖNCÜ HESABINIZDAN İP ADRESİNİZİ TANIMLAMANIZ GEREKMEKTEDİR.
+// APİLERDEN GELEN CEVAP JSON ÇIKTISIDIR. Örnek Çıktı: [true,"İşlem Yapıldı"]
+// APİ ERİŞİMİNİ KULLANIMA SUNAN YÖNCÜYE TEŞEKKÜR EDERİM.
+
+$apilinki = 'http://www.yoncu.com/apiler/sunucu/power.php'; // APİ VERİ ALIP GÖNDERME İŞLEMİ BİZ SUNUCU KAPAMA AÇMA İŞLEMİNE VERİ YOLLAYIP ALACAĞIZ
+$apikullanicinumarasi = '2'; // api kullanıcı numaranız
+$apisifresi = 'sifreniz'; // api şifreniz
+$sunucunumarasi = 'örnek 1'; // sunucu numarası yani işlem numarasi
+$islemturu $_POST['islemturu']; // yapıcağınız işlem türü örneğin restart atmak için reset komutunu yollıyabilirsiniz
+
+//
+if (isset($_POST["Gonder"])) {
+	
+$veri = curl_init();
+curl_setopt($veri, CURLOPT_URL, ''.$apilinki.''.$apisifresi.''.$sunucunumarasi.''.$islemturu.'');
+curl_setopt($veri, CURLOPT_HEADER, false);
+curl_setopt($veri, CURLOPT_ENCODING, false);
+curl_setopt($veri, CURLOPT_COOKIESESSION, false);
+curl_setopt($veri, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($veri, CURLOPT_HTTPHEADER, array(
+	'Connection: keep-alive',
+	'User-Agent: '.$_SERVER['SERVER_NAME'],
+	'Referer: http://www.yoncu.com/',
+	'Cookie: YoncuKoruma='.$_SERVER['SERVER_ADDR'].';YoncuKorumaRisk=0',
+));
+
+}
+
+?>
+
+
+<form action="" method="post">
+
+     <div class="mesaj"></div>
+
+
+<div class="form-group">
+                     <label for="islem" class="">İşlem</label>
+                     <input type="text" name="islemturu" class="form-control" id="islemturu" placeholder="Reset" required>
+                  </div>
+          
+
+ <hr>
+  <a href="index.php" class="btn btn-danger pull-left">
+         Geri Dön
+  </a>
+  <button type="submit" name="Gonder" class="btn btn-success pull-right">
+         İşlemi Yolla
+  </button>
+</div>
